a45055211a5b8daa6c135d05d5b4b1dd892ea112
tobiolutimehin <olutimehintobi@gmail.com>
2024-01-06 19:38:12 -0500

    upgrade gradle
    Signed-off-by: tobiolutimehin <olutimehintobi@gmail.com>

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -31,14 +33,49 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
 }
 
 dependencies {
-
+    implementation(libs.activity.compose)
     implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
+    implementation(libs.material3)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ui.test.junit4)
+    androidTestImplementation(platform(libs.compose.bom))
+
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.material3.adaptive.navigation.suite)
+    implementation(libs.material3.windowSizeClass)
+    implementation(libs.material3.adaptive)
+
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    // Modules
+    implementation(project(mapOf("path" to ":core:network")))
+    implementation(project(mapOf("path" to ":core:data")))
+    implementation(project(mapOf("path" to ":core:designsystem")))
 }
