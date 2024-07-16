389b3bb6307d67293c3681a61ee244ce04ad2959
tobiolutimehin <olutimehintobi@gmail.com>
2024-07-16 18:44:52 -0400

    Add the datastore to store saved ids
    Signed-off-by: tobiolutimehin <olutimehintobi@gmail.com>

--- core/network/src/main/java/com/veritalex/core/network/api/NetworkDataSource.kt
+++ core/network/src/main/java/com/veritalex/core/network/api/NetworkDataSource.kt
@@ -1,20 +1,28 @@
 package com.veritalex.core.network.api
 
-import com.veritalex.core.network.models.BookListResponse
 import com.veritalex.core.network.models.BookDto
+import com.veritalex.core.network.models.BookListResponse
 import retrofit2.Response
 import javax.inject.Inject
 
 interface NetworkDataSource {
-    suspend fun getBooks(page: Int? = null): Response<BookListResponse>
+    suspend fun getBooks(
+        page: Int? = null,
+        ids: Set<Int>? = null,
+    ): Response<BookListResponse>
 
     suspend fun getBookById(id: Int): Response<BookDto>
 }
 
-class RetrofitNetworkDataSource @Inject constructor(
-    private val apiService: GutendexApiService,
-) : NetworkDataSource {
-    override suspend fun getBooks(page: Int?): Response<BookListResponse> = apiService.getBooks(page = page)
+class RetrofitNetworkDataSource
+    @Inject
+    constructor(
+        private val apiService: GutendexApiService,
+    ) : NetworkDataSource {
+        override suspend fun getBooks(
+            page: Int?,
+            ids: Set<Int>?,
+        ): Response<BookListResponse> = apiService.getBooks(page = page, ids = ids.toString())
 
-    override suspend fun getBookById(id: Int): Response<BookDto> = apiService.getBookById(id)
+        override suspend fun getBookById(id: Int): Response<BookDto> = apiService.getBookById(id)
 }
