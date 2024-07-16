139126e93b0c992f6c7532aa53a32b5bd568c9e1
tobiolutimehin <olutimehintobi@gmail.com>
2024-07-16 18:45:52 -0400

    update offline repository
    Signed-off-by: tobiolutimehin <olutimehintobi@gmail.com>

--- core/data/src/main/java/com/veritalex/core/data/repository/OfflineFirstBooksRepository.kt
+++ core/data/src/main/java/com/veritalex/core/data/repository/OfflineFirstBooksRepository.kt
@@ -9,9 +9,13 @@ import com.veritalex.core.data.models.Book
 import com.veritalex.core.data.utils.Extensions.insertBooks
 import com.veritalex.core.data.utils.ModelConverters.toBook
 import com.veritalex.core.database.dao.BookDao
+import com.veritalex.core.datastore.SavedBooksPreferencesDatastore
 import com.veritalex.core.network.api.RetrofitNetworkDataSource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
@@ -21,29 +25,39 @@ interface BooksRepository {
     fun fetchBooks(): Flow<PagingData<Book>>
 
     fun fetchRecommendedBooks(topic: String? = null): Flow<List<Book>>
+
+    suspend fun saveBook(id: String)
+
+    fun fetchSavedBooks(): Flow<List<Book>> // Change this to non-suspend
 }
 
-@OptIn(ExperimentalPagingApi::class)
+@OptIn(ExperimentalPagingApi::class, ExperimentalCoroutinesApi::class)
 class OfflineFirstBooksRepository
     @Inject
     constructor(
         private val network: RetrofitNetworkDataSource,
         private val bookDao: BookDao,
+        private val savedBooksDatastore: SavedBooksPreferencesDatastore,
     ) : BooksRepository {
-        override fun fetchRecommendedBooks(topic: String?): Flow<List<Book>> {
-            return bookDao.getRecommendedStream(topic).map { booksEntities ->
-                booksEntities.map { bookEntity ->
-                    bookEntity.toBook()
-                }
-            }.onEach { books ->
-                if (books.isEmpty()) {
-                    withContext(Dispatchers.IO) { getBooks() }
-                }
-            }
+        override suspend fun saveBook(id: String) {
+            savedBooksDatastore.updateBookIds(id)
         }
 
-        override fun fetchBooks(): Flow<PagingData<Book>> {
-            return Pager(
+        override fun fetchRecommendedBooks(topic: String?): Flow<List<Book>> =
+            bookDao
+                .getRecommendedStream(topic)
+                .map { booksEntities ->
+                    booksEntities.map { bookEntity ->
+                        bookEntity.toBook()
+                    }
+                }.onEach { books ->
+                    if (books.isEmpty()) {
+                        withContext(Dispatchers.IO) { getBooks() }
+                }
+            }
+
+    override fun fetchBooks(): Flow<PagingData<Book>> =
+        Pager(
                 config =
                     PagingConfig(
                         pageSize = 100,
@@ -58,10 +72,24 @@ class OfflineFirstBooksRepository
                     bookEntity.toBook()
                 }
             }
+
+    override fun fetchSavedBooks(): Flow<List<Book>> =
+        savedBooksDatastore.savedBookIds.flatMapLatest { savedBookIds ->
+            bookDao.getBookWithIds(savedBookIds.toSet()).flatMapLatest { booksEntities ->
+                val foundBooks = booksEntities.map { it.book.bookId }.toSet()
+                val missingIds = savedBookIds.map { it.toInt() }.toSet() - foundBooks
+                if (missingIds.isNotEmpty()) {
+                    getBooks(missingIds)
+                }
+                flowOf(booksEntities.map { it.toBook() })
+            }
         }
 
-        private suspend fun getBooks() {
-            val response = network.getBooks()
+    private suspend fun getBooks(ids: Set<Int>? = null) {
+        val response =
+            network.getBooks(
+                ids = ids,
+            )
             if (response.isSuccessful) {
                 response.body()?.results?.insertBooks(bookDao)
             }
